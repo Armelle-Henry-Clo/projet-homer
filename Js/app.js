414eaf9aa098a5ed0ab0b7920d487596b5571d94
Armelle HENRY_Clo81 <clotildearmelle@gmail.com>
2022-02-19 16:01:03 +0100

    iframe

--- Js/app.js
+++ Js/app.js
@@ -1,31 +1,19 @@
-   
     let image = document.getElementById('head');
     let img = image.getAttribute('src');
+
 // ma fonction-------------  
-
-
-// console.log(img);
-    
-    function carrousel(img){
-        
         switch('src'){
-            case 'img/simpson1.jpeg':
+            case 'img/simpson2.jpeg':
                 img.setAttribute('src','img/simpson5.jpeg');
             break;
             case 'img/simpson5.jpeg':
+                console.log(img);
                 img.setAttribute('src','img/zapette.jpeg');
             break;
             case 'img/zapette.jpeg':
-                img.setAttribute('src','img/simpson2.jpeg');
-            break;
-            case 'img/simpson2.jpeg':
                 img.setAttribute('src','img/simpson1.jpeg');
             break;
-        }
-        
-       
-        // console.log(img); 
-    }
-    
-    
-//   console.log(carrousel);
+            case 'img/simpson1.jpeg':  
+                img.setAttribute('src','img/simpson2.jpeg');
+            break;   
+        };
